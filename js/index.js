e41f8614658492c529026541305df7faac48f225
Vania Atanasova <vaniaatanasova83@gmail.com>
2018-08-03 15:17:01 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("home");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
